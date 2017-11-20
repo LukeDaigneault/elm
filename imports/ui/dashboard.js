11969a1195f0a1c74ce6b6898b7e65f1f686c7b7
Luke Daigneault <Luke.Daigneault@gmail.com>
2017-11-20 21:34:42 +0100

    added db lookup

--- imports/ui/dashboard.js
+++ imports/ui/dashboard.js
@@ -1,4 +1,12 @@
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 
+import { Interfaces } from '../api/interfaces.js';
+
 import './dashboard.html';
+
+Template.dashboard.helpers({
+  interfaces() {
+    return Interfaces.find({});
+  }
+});
