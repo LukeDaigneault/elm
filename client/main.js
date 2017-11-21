ba0df0433f00515591c431f81072cf8aac8b7e34
Luke Daigneault <luke.daigneault@gmail.com>
2017-11-21 14:09:42 +0100

    add routes and global helper

--- client/main.js
+++ client/main.js
@@ -1,1 +1,17 @@
 import '../imports/ui/body.js';
+
+if(Meteor.isClient) {
+  Router.route('/',function(){
+      this.render('body');
+  });
+  Router.route('/ManageInterfaces',function(){
+      this.render('interface', {data: {navinterface: 'active'}});
+  });
+    Router.route('/Dashboard',function(){
+        this.render('dashboard',{data: {navdashboard: 'active'}});
+    });
+}
+
+Template.registerHelper('currentRouteIs', function (route) {
+  return Router.current().route.getName() === route;
+});
