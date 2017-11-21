282dd334d8534d1061d78a50c7258d5d9b4f4946
Luke Daigneault <luke.daigneault@gmail.com>
2017-11-21 11:58:49 +0100

    Added ability to add and delete interfaces

--- imports/ui/dashboard.js
+++ imports/ui/dashboard.js
@@ -1,12 +1,5 @@
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 
-import { Interfaces } from '../api/interfaces.js';
 
 import './dashboard.html';
-
-Template.dashboard.helpers({
-  interfaces() {
-    return Interfaces.find({});
-  }
-});
--- imports/ui/interface.js
+++ imports/ui/interface.js
@@ -1,4 +1,46 @@
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
+import { Interfaces } from '../api/interfaces.js';
 
 import './interface.html';
+
+Template.interface.helpers({
+  interfaces() {
+    return Interfaces.find({}, {sort: {interfaceName: 1} });
+  }
+});
+
+Template.interface.events({
+  'submit .new-interface'(event) {
+    // Prevent default browser form submit
+    event.preventDefault();
+
+    // Get value from form element
+    const target = event.target;
+    const interfaceName = target.interfaceName.value;
+    const fDNumber = target.fDNumber.value;
+    const email = target.email.value;
+    const facebook = target.facebook.value;
+    const sms = target.sms.value;
+
+    // Insert a task into the collection
+    Interfaces.insert({
+      interfaceName: interfaceName,
+      fDNumber: fDNumber,
+      email: email,
+      facebook: facebook,
+      sms: sms,
+      createdAt: new Date(), // current time
+    });
+
+    // Clear form
+    target.interfaceName.value = '';
+    target.fDNumber.value = '';
+    target.email.value = '';
+    target.facebook.value = '';
+    target.sms.value = '';
+  },
+  'click .delete-interface'() {
+    Interfaces.remove(this._id);
+  },
+});
