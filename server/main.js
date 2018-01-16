d2144b3bcbcbdd8aec80fb348a0d553b09ef7594
Luke Daigneault <luke.daigneault@gmail.com>
2018-01-16 23:08:20 +0100

    added mailgun stuff

--- server/main.js
+++ server/main.js
@@ -3,7 +3,9 @@ import '../imports/api/profiles.js';
 import '../imports/api/alerts.js';
 import '../imports/api/twilio.js';
 
-
+Meteor.startup(function () {
+  process.env.MAIL_URL = Meteor.settings.mailgunURL;
+});
 
 
 if (Meteor.isServer) {
@@ -26,6 +28,13 @@ if (Meteor.isServer) {
             this.response.end(JSON.stringify(response));
 
             Meteor.call('sms.send', this.request.body.alert);
+
+            Email.send({
+                to: "luke.daigneault@gmail.com",
+                from: "postmaster@sandbox60d6dfd3ac9f44f28305a177bf6da2a4.mailgun.org",
+                subject: "Example Email",
+                text: "The contents of our email in plain text.",
+              });
         }
 
 
