b701312fafe023c070f3db1c2a1e85f975950afb
Luke Daigneault <luke.daigneault@gmail.com>
2018-02-12 22:14:34 +0100

    Fixed SMS sending

--- imports/api/twilio.js
+++ imports/api/twilio.js
@@ -1,5 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import { Interfaces } from './interfaces.js';
+import { Components } from './components.js';
 import { Profiles } from './profiles.js';
 var twilio = require('twilio');
 
@@ -10,31 +11,37 @@ if(Meteor.isServer){
 
               const twilioAccountId = Meteor.settings.twilioAccountId;
               const twilloAuthtoken = Meteor.settings.twilloAuthtoken;
-              const smsOwners = Interfaces.find({flowIdentifier: alert.flowIdentifier, sms: true}, {owner:1,  _id:0});
+              const componentInterfaces = Components.find({componentName: alert.componentName}, {interfaceOwner:1, _id:0});
 
-              const owners = [];
+              componentInterfaces.forEach(function (compDoc) {
 
-              smsOwners.forEach(function(doc){
-                owners.push(doc.owner);
-              });
+                const smsOwners = Interfaces.find({_id: compDoc.interfaceOwner, sms: true}, {interfaceName:1, owner:1,  _id:0});
 
-              const smsNumbers = Profiles.find({owner: {$in: owners}}, {phoneNumber:1, _id:0});
+                smsOwners.forEach(function(intDoc){
 
+                  const smsNumbers = Profiles.find({owner: intDoc.owner}, {phoneNumber:1, _id:0});
 
-              smsNumbers.forEach(function(doc){
+                  smsNumbers.forEach(function(ownerDoc){
 
-              try {
-                    if (twilioAccountId && twilloAuthtoken){
-                       var client = new twilio(twilioAccountId, twilloAuthtoken);
-                       client.messages.create({
-                       body: 'An error has occured with Interface ' + alert.flowIdentifier +  ' transaction ID: ' + alert.transactionId,
-                        to: doc.phoneNumber,  // Text this number
-                        from: '+33756798817' // From a valid Twilio number
-                      });
-                   }
-              } catch (err) {
-                throw new Meteor.error(err);
-              }
+                    try {
+                          if (twilioAccountId && twilloAuthtoken){
+                             var client = new twilio(twilioAccountId, twilloAuthtoken);
+                             client.messages.create({
+                             body: 'An error has occured with Interface "' +
+                              intDoc.interfaceName +
+                              '" in component "' +
+                              alert.componentName +
+                              '" , transaction ID: ' +
+                              alert.transactionId,
+                             to: ownerDoc.phoneNumber,  // Text this number
+                             from: '+33756798817' // From a valid Twilio number
+                            });
+                         }
+                    } catch (err) {
+                      throw new Meteor.error(err);
+                    }
+                  });
+                });
               });
         }
 
