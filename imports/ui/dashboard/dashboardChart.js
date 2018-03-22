299bb392efba17371bfc8274457fbb66d6e54c92
Luke Daigneault <luke.daigneault@gmail.com>
2018-03-22 23:44:56 +0100

    reactive chart now working

--- imports/ui/dashboard/dashboardChart.js
+++ imports/ui/dashboard/dashboardChart.js
@@ -1,106 +1,140 @@
 import { Template } from 'meteor/templating';
 import { Chart } from 'chart.js/src/chart.js';
 import { Alerts } from '../../api/alerts.js';
-//import { Components} from '../../api/components.js'; 
+//import { Components} from '../../api/components.js';
 import './dashboardChart.html';
 
-Template.body.onCreated(function() {
+function buildDataSeries(datalabels, datavalues) {
+  console.log('inside buildDataSeries');
+  const now = new Date();
 
-});
+  const lookupStartDate = new Date();
+  lookupStartDate.setMonth(lookupStartDate.getMonth() - 1);
+  lookupStartDate.setHours(0, 0, 0, 0);
 
+  const lookupEndDate = new Date();
+  lookupEndDate.setMonth(lookupEndDate.getMonth() - 1);
+  lookupEndDate.setHours(24, 0, 0, 0);
+  //for (i = 6; i >= 0; i--) {
+  while (lookupStartDate < now) {
+    datalabels.push(moment(lookupStartDate).format('ddd Do MMM'));
 
-Template.dashboardChart.onRendered(function() {
-  const alerts = this.subscribe("alerts");
-
-  Tracker.autorun(function() {
-    if (alerts.ready()) {
-      const datalabels = [];
-      const datavalues = [];
-
-      //const components = Components.find({}, {componentName:1,  _id:0});
-      //const componentNames = [];
-
-      //components.forEach(function(doc){
-      //  componentNames.push(doc.componentName);
-      //});
-
-      const now = new Date();
-
-      const lookupStartDate = new Date();
-      lookupStartDate.setMonth(lookupStartDate.getMonth() - 1);
-      lookupStartDate.setHours(0, 0, 0, 0);
-
-      const lookupEndDate = new Date();
-      lookupEndDate.setMonth(lookupEndDate.getMonth() - 1);
-      lookupEndDate.setHours(24, 0, 0, 0);
-      //for (i = 6; i >= 0; i--) {
-      while (lookupStartDate < now) {
-        datalabels.push(moment(lookupStartDate).format('ddd Do MMM'));
-
-        datavalues.push(
-          Alerts.find({
-            //        componentName: {
-            //        $in: componentNames
-            //        },
-            createdAt: {
-              $gte: lookupStartDate,
-              $lte: lookupEndDate,
-            },
-          }).count()
-        );
-
-        lookupStartDate.setDate(lookupStartDate.getDate() + 1);
-        lookupEndDate.setDate(lookupEndDate.getDate() + 1);
-      }
-      const ctx = document.getElementById("dashboardChart");
-      const myChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-          labels: datalabels,
-          datasets: [{
-            label: 'Alerts',
-            data: datavalues,
-            fill: false,
-            borderColor: 'rgba(255,99,132,1)',
-            backgroundColor: 'rgba(255,99,132,1)',
-            borderWidth: 1
-          }]
+    datavalues.push(
+      Alerts.find({
+        //        componentName: {
+        //        $in: componentNames
+        //        },
+        createdAt: {
+          $gte: lookupStartDate,
+          $lte: lookupEndDate,
         },
-        options: {
-          legend: {
-            display: false
-          },
-          responsive: true,
-          tooltips: {
-            mode: 'index',
-            intersect: false,
-          },
-          hover: {
-            mode: 'nearest',
-            intersect: true
-          },
-          scales: {
-            xAxes: [{
-              display: true,
-              scaleLabel: {
-                display: false,
-                labelString: 'Date'
-              }
-            }],
-            yAxes: [{
-              display: true,
-              ticks: {
-                beginAtZero: true
-              },
-              scaleLabel: {
-                display: true,
-                labelString: 'Number of Alerts'
-              }
-            }]
+      }).fetch().length
+    );
+
+    lookupStartDate.setDate(lookupStartDate.getDate() + 1);
+    lookupEndDate.setDate(lookupEndDate.getDate() + 1);
+  }
+}
+
+Template.dashboardChart.onRendered(function chartOnRendered() {
+  console.log('inside onRendered');
+  this.datalabels = [];
+  this.datavalues = [];
+
+  const ctx = document.getElementById("dashboardChart");
+  const myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: this.datalabels,
+      datasets: [{
+        label: 'Alerts',
+        data: this.datavalues,
+        fill: false,
+        borderColor: 'rgba(255,99,132,1)',
+        backgroundColor: 'rgba(255,99,132,1)',
+        borderWidth: 1
+      }]
+    },
+    options: {
+      legend: {
+        display: false
+      },
+      responsive: true,
+      tooltips: {
+        mode: 'index',
+        intersect: false,
+      },
+      hover: {
+        mode: 'nearest',
+        intersect: true
+      },
+      scales: {
+        xAxes: [{
+          display: true,
+          scaleLabel: {
+            display: false,
+            labelString: 'Date'
           }
-        }
-      });
+        }],
+        yAxes: [{
+          display: true,
+          ticks: {
+            beginAtZero: true
+          },
+          scaleLabel: {
+            display: true,
+            labelString: 'Number of Alerts'
+          }
+        }]
+      }
     }
   });
 
+
+  this.subscribe('alerts', () => { // removes the need for an onReady() test
+    // code to populate this.data array (find().fetch()) and render initial chart
+    console.log('inside subscribe');
+    buildDataSeries(this.datalabels, this.datavalues);
+
+    myChart.update();
+
+    this.autorun(() => {
+      // template autoruns will be cleaned up for me
+      // code to update this.data (find().fetch()) from the collection and re-draw series (this is a lightweight operation).
+      console.log('inside AutoRun');
+
+      const oneMonthOfAlerts = new Date();
+      oneMonthOfAlerts.setMonth(oneMonthOfAlerts.getMonth() - 1);
+      oneMonthOfAlerts.setHours(0, 0, 0, 0);
+
+
+
+      var myCursor = Alerts.find({
+        createdAt: {
+          $gte: oneMonthOfAlerts,
+        },
+      }).fetch()
+
+
+      while (this.datalabels.length > 0) {
+        this.datalabels.pop();
+      }
+
+      while (this.datavalues.length > 0) {
+        this.datavalues.pop();
+      }
+
+      buildDataSeries(this.datalabels, this.datavalues);
+
+      myChart.update();
+    });
+  });
 });
+
+
+//const components = Components.find({}, {componentName:1,  _id:0});
+//const componentNames = [];
+
+//components.forEach(function(doc){
+//  componentNames.push(doc.componentName);
+//});
